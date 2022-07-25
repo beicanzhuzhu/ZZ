84b22e4ef75074601dd238c6113f0f1713f633ff
beicanyuzhuzhu <beicanzhuzhu@gmail.com>
2022-07-25 20:17:49 +0800

    readme

--- sever/sever.py
+++ sever/sever.py
@@ -13,6 +13,7 @@ class Sever:
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.bind((host, port))
         self.s.listen(3)
+        self.__conns = []
 
     @property
     def __zz_id(self):  # 还需修改，防止重复
@@ -41,7 +42,11 @@ class Sever:
             massages = request.split(",")
             
             # 开始处理消息
-            
+
+            # 先将用户加入连接列表
+            if massages[0] != 0 and {"zz_id": massages[1], "conn": conn} not in self.__conns:
+                self.__conns.append({"zz_id": massages[1], "conn": conn})
+
             # 注册
             # [0, user_name, password, ip]
             if massages[0] == "0":
@@ -49,6 +54,8 @@ class Sever:
                 with open("users.json", "r") as u:
                     a = json.load(u)
                     a.append({"name": massages[1], "zz_id": zz_id, "password": massages[2]})
+                # 将用户加入连接列表
+                self.__conns.append({"zz_id": massages[1], "conn": conn})
                     
             # 登陆
             # [1, zz_id, password, ip]
@@ -64,10 +71,8 @@ class Sever:
             # [3, zz_id]
             elif massages[0] == "3":
                 pass
-                
-                 
 
-    def wait_handle_request(self):
+    def start(self):
 
         while True:
             # 等待连接
@@ -75,6 +80,5 @@ class Sever:
             # 接收连接并分配线程处理
             thread = threading.Thread(target=self.__handle_request, args=(conn, port))
             # 守护线程
-            thread.setDaemon(True)
+            thread.daemon = True
             thread.start()
-
