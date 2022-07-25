b8bcaa84347136c991b356801da5fad87abf0311
beicanyuzhuzhu <beicanzhuzhu@gmail.com>
2022-07-25 21:03:41 +0800

    readme

--- sever/sever.py
+++ sever/sever.py
@@ -10,9 +10,9 @@ class Sever:
     def __init__(self, host, port):
 
         # 初始化服务器
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.bind((host, port))
-        self.s.listen(3)
+        self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__s.bind((host, port))
+        self.__s.listen(3)
         self.__conns = []
 
     @property
@@ -21,6 +21,8 @@ class Sever:
         return randint(10000, 99999)
 
     def __handle_request(self, conn, port):
+
+        print(port, "已连接")
 
         while True:
             # 接收用户端消息
@@ -31,10 +33,10 @@ class Sever:
                 # 将用户状态设为 0 (下线)
                 break
             # 打印有关消息
-            print("来自 ", port, " 的链接 : [", request, "]")
+            print("来自 ", port, " 的消息 : [", request, "]")
             # 将消息分割为列表
             massages = request.split(",")
-            
+
             # 开始处理消息
 
             # 先将用户加入连接列表
@@ -50,17 +52,17 @@ class Sever:
                     a.append({"name": massages[1], "zz_id": zz_id, "password": massages[2]})
                 # 将用户加入连接列表
                 self.__conns.append({"zz_id": massages[1], "conn": conn})
-                    
+
             # 登陆
             # [1, zz_id, password, ip]
             elif massages[0] == "1":
                 pass
-            
+
             # 添加好友
             # [2, zz_id_1, zz_id_2]
             elif massages[0] == "2":
                 pass
-            
+
             # 获取好友列表
             # [3, zz_id]
             elif massages[0] == "3":
@@ -75,9 +77,14 @@ class Sever:
 
         while True:
             # 等待连接
-            conn, port = self.s.accept()
+            conn, port = self.__s.accept()
             # 接收连接并分配线程处理
             thread = threading.Thread(target=self.__handle_request, args=(conn, port))
             # 守护线程
             thread.daemon = True
             thread.start()
+
+
+if __name__ == '__main__':
+    sever = Sever("", 8080)
+    sever.start()
