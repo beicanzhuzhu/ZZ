76aec6e9246e0b4c21c31e34a8363b021b62facb
beicanyuzhuzhu <beicanzhuzhu@gmail.com>
2022-07-27 00:01:48 +0800

    user_side

--- user/user.py
+++ user/user.py
@@ -10,6 +10,24 @@ class User:
     def __init__(self, host, port):
         self._s = socket.socket()
         self._s.connect((host, port))
+        self._information = {"name": "", "zz_id": "",
+                             "friends": [], "friend_request": [],
+                             }
+
+    def _show_main_window(self):
+        pass
+
+    def _show_friends(self):
+        pass
+
+    def _show_friend_request(self):
+        pass
+
+    def _show_chat_interface(self):
+        pass
+
+    def receive(self):
+        pass
 
     def log_in(self):
         pass
@@ -18,3 +36,5 @@ class User:
         pass
 
 
+if __name__ == "__main__":
+    pass
